c12fc95f6dd78fcd5cd7fa41bc9d93d94635c5ba
Kyle Smith <smith.kyle.r93@gmail.com>
2019-05-21 22:56:36 -0700

    Added giphy-browser

---
+++ giphy-browser/precache-manifest.dcc32820e75f61666383ecc5746a2d05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ca6a8330e698f2486fa55d8158427b",
+    "url": "/giphy-browser/index.html"
+  },
+  {
+    "revision": "0a3dbf86ff29b852bdd3",
+    "url": "/giphy-browser/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "937fe1271bd11271f6b6",
+    "url": "/giphy-browser/static/js/2.760d7afc.chunk.js"
+  },
+  {
+    "revision": "0a3dbf86ff29b852bdd3",
+    "url": "/giphy-browser/static/js/main.d2d1badd.chunk.js"
+  },
+  {
+    "revision": "68026254e360462bbb44",
+    "url": "/giphy-browser/static/js/runtime~main.a60fd64a.js"
+  }
+]);
